21e4ac80c8e0c3bd1ebebf7649333cc780e48c55
Francisco Vicente <franciscogsvicente@gmail.com>
2024-11-16 16:50:22 +0000

    Updated prod adid

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://porto-metro-api.onrender.com',
-  adId: 'my-add-id-prod',
+  adId: 'ca-app-pub-8772237511019433/2285464208',
 };
